484334a0efd489ef65b898b2afa1d2f1021625a6
Nasser-96 <nasserabunasir96@outlook.com>
2022-01-23 16:59:59 +0300

    Refactor Files

--- MyInterests/src/main/java/com/example/MyInterests/Comment/CommentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.MyInterests.Comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment,Long> {
-}
